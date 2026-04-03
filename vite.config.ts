08a4f6b09bcd30147238c0670534ab27a7a7cd81
Douglas Barrera <53149834+Doisaac@users.noreply.github.com>
2026-04-03 12:18:54 -0600

    feat: add project base

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import babel from '@rolldown/plugin-babel'
+import path from 'path'
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    babel({ presets: [reactCompilerPreset()] }),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+})
