68cbd3c257fb68240a1710bb9239241e38dbaa9b
Douglas Barrera <53149834+Doisaac@users.noreply.github.com>
2026-04-17 15:32:28 -0600

    fix: update queryKey in useEpisodes to correctly reference episodes

--- src/characters/hooks/useEpisodes.tsx
+++ src/characters/hooks/useEpisodes.tsx
@@ -3,7 +3,7 @@ import { getEpisodesAction } from '@/characters/actions/getEpisodesAction'
 
 export const useEpisodes = (episodesId: number[]) => {
   return useQuery({
-    queryKey: ['character', episodesId],
+    queryKey: ['episodes', episodesId],
     queryFn: () => getEpisodesAction(episodesId.slice(0, 10)),
     retry: false,
     staleTime: 1000 * 60 * 5,
