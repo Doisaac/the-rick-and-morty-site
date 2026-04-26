57ff2ae7af37c6db8cd651b5404b60aace2f4515
Douglas Barrera <53149834+Doisaac@users.noreply.github.com>
2026-04-25 22:31:33 -0600

    style: update CustomJumbotron styles for improved text alignment and font weight

--- src/components/custom/CustomJumbotron.tsx
+++ src/components/custom/CustomJumbotron.tsx
@@ -5,9 +5,9 @@ interface Props {
 
 export const CustomJumbotron = ({ title, subtitle }: Props) => {
   return (
-    <section className="flex items-center justify-center flex-col gap-3 text-white">
-      <h1 className="font-bold text-6xl">{title}</h1>
-      <p className="font-semibold text-white/70">{subtitle}</p>
+    <section className="flex items-center justify-center flex-col gap-3 text-white text-center">
+      <h1 className="font-black text-6xl">{title}</h1>
+      <p className="font-semibold text-white/70 text-lg">{subtitle}</p>
     </section>
   )
 }
